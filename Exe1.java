590a4b806a409645903ef1d3988e45bdc843a2c8
Lucelly Santos <lucellybezerra@gmail.com>
2017-06-02 22:37:26 -0300

    Add files via upload

---
+++ Exe1.java
@@ -0,0 +1,27 @@
+package so.aula10.entradasaida;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class Exe1 {
+    public static void main(String[] args) throws IOException {
+
+            Scanner entrada = new Scanner(new FileInputStream(FileDescriptor.in));
+
+            PrintWriter saida = new PrintWriter (new FileOutputStream(FileDescriptor.out));
+
+            saida.println("Digite o seu nome: ");
+            saida.flush();
+            String nome = entrada.nextLine();
+            saida.println("Prazer em conhece-lo, " + nome + "!");
+            saida.flush();
+
+            entrada.close();
+            saida.close();
+    }
+
+}
+    
